b4a11ddd49b4d044428006eb9feecc7421fcb1aa
alexey_yunoshev <alexey.yunoshev@gmail.com>
2019-05-19 12:30:48 +0300

    add more routes

--- app/src/api/post.js
+++ app/src/api/post.js
@@ -10,4 +10,35 @@ router.post('/', async (req, res) => {
     res.status(201).json(post);
 });
 
+router.get('/:id', async (req, res) => {
+    const post = await Post.findByPk(req.params.id);
+    if (!post) {
+        res.sendStatus(404);
+    } else {
+        res.json(post);
+    }
+});
+
+router.put('/:id', async (req, res) => {
+    const [affectedCount] = await Post.update(req.body, {
+        where: {
+            id: req.params.id,
+        }
+    });
+    if (affectedCount === 0) {
+        res.sendStatus(404);
+    } else {
+        res.sendStatus(200);
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    await Post.destroy({
+        where: {
+            id: req.params.id
+        }
+    });
+    res.send(200);
+});
+
 module.exports = router;
